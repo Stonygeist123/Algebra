bfba9120282360078b9b9e96b17dcfa8ed92a242
Stonygeist123 <65509673+Stonygeist123@users.noreply.github.com>
2023-11-27 16:40:03 +0100

    Clean.

--- Algorithms/Roots.cs
+++ Algorithms/Roots.cs
@@ -26,39 +26,6 @@ namespace Algebra.Algorithms
             if (MathF.Abs(y) <= Zero)
                 return new(x, y);
             return null;
-
-            /*if (MathF.Abs(p1.Y) <= Zero)
-                return p1;
-            else if (MathF.Abs(p2.Y) <= Zero)
-                return p2;
-
-            if (p1.Y <= Zero && p2.Y <= Zero ||
-                p1.Y >= Zero && p2.Y >= Zero)
-                return null;
-
-            float a = p1.X,
-                b = p2.X,
-                c = (a + b) / 2;
-            float c_ev = fn.Evaluate(symbols, c);
-            for (int i = 0; i < 15; ++i)
-            {
-                float b_ev = fn.Evaluate(symbols, b),
-                    a_ev = fn.Evaluate(symbols, a);
-                if (c_ev == 0)
-                    return new(c, c_ev);
-
-                if (c_ev < 0 && b_ev > 0 || c_ev > 0 && b_ev < 0)
-                    a = c;
-                else if (c_ev < 0 && a_ev > 0 || c_ev > 0 && a_ev < 0)
-                    b = c;
-
-                c = (a + b) / 2;
-                c_ev = fn.Evaluate(symbols, c);
-            }
-
-            if (MathF.Abs(fn.Evaluate(symbols, c)) <= Zero)
-                return new(c, c_ev);
-            return null;*/
         }
     }
 }
